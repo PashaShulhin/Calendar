e1b24c970a95a319d8968fe5f994eb49241f9669
Pavlo Shulhin <pavelshulgin2009@gmail.com>
2025-01-15 18:53:07 -0500

    fixed 1.3

--- Task 1.6/script.js
+++ Task 1.6/script.js
@@ -1,11 +1,11 @@
 const descriptions = {
     "introduction": "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-    "Prerequisutes": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined.",
-    "Modules": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
-    "Solving common CSS problems": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
-    "Working with CSS": "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, etc.",
-    "Adding CSS": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
-    "See also": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined."
+    "prerequisutes": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined.",
+    "modules": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    "solvingCommonCssProblems": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    "workingWithCss": "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, etc.",
+    "addingCss": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words.",
+    "seeAlso": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined."
 };
 
 function updateDescription(displayType) {
@@ -17,26 +17,26 @@ document.getElementById("introduction").addEventListener('click', function() {
     updateDescription("introduction");
 });
 
-document.getElementById("Prerequisutes").addEventListener('click', function() {
-    updateDescription("Prerequisutes");
+document.getElementById("prerequisutes").addEventListener('click', function() {
+    updateDescription("prerequisutes");
 });
 
-document.getElementById("Modules").addEventListener('click', function() {
-    updateDescription("Modules");
+document.getElementById("modules").addEventListener('click', function() {
+    updateDescription("modules");
 });
 
-document.getElementById("Solving common CSS problems").addEventListener('click', function() {
-    updateDescription("Solving common CSS problems");
+document.getElementById("solvingCommonCssProblems").addEventListener('click', function() {
+    updateDescription("solvingCommonCssProblems");
 });
 
-document.getElementById("Working with CSS").addEventListener('click', function() {
-    updateDescription("Working with CSS");
+document.getElementById("workingWithCss").addEventListener('click', function() {
+    updateDescription("workingWithCss");
 });
 
-document.getElementById("Adding CSS").addEventListener('click', function() {
-    updateDescription("Adding CSS");
+document.getElementById("addingCss").addEventListener('click', function() {
+    updateDescription("addingCss");
 });
 
-document.getElementById("See also").addEventListener('click', function() {
-    updateDescription("See also");
+document.getElementById("seeAlso").addEventListener('click', function() {
+    updateDescription("seeAlso");
 });
