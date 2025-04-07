280b541391bc589b8a6cf09984e919f2b6299ef7
Pavlo <pavelshulgin2009@gmail.com>
2025-04-07 19:46:53 -0400

    fixed (put a Timer in the folder)

--- task 3.2/src/App.jsx
+++ task 3.2/src/App.jsx
@@ -1,48 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Timer from "./components/Timer.jsx";
 
-function Timer() {
-  const [seconds, setSeconds] = useState(0); 
-  let timer; 
-
-  const formatTime = (totalSeconds) => {
-    const minutes = Math.floor(totalSeconds / 60);
-    const secs = totalSeconds % 60;
-    return `${minutes < 10 ? "0" : ""}${minutes}:${
-      secs < 10 ? "0" : ""
-    }${secs}`;
-  };
-
-  const startTimer = () => {
-    if (timer) return; 
-    timer = setInterval(() => {
-      setSeconds((prev) => prev + 1);
-    }, 1000);
-  };
-
-  const stopTimer = () => {
-    clearInterval(timer);
-    timer = null;
-  };
-
-  const resetTimer = () => {
-    stopTimer();
-    setSeconds(0);
-  };
-
-  useEffect(() => {
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+function App() {
   return (
     <div>
-      <h1>{formatTime(seconds)}</h1>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-      <button onClick={resetTimer}>Reset</button>
+      <Timer />
     </div>
   );
 }
 
-export default Timer;
+export default App;
