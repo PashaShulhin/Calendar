806c3409c72f59083d2580445307cf1c51090727
Pavlo Shulhin <pavelshulgin2009@gmail.com>
2025-08-03 22:10:53 -0400

    fixed

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import eventReducer from 'entities/event/eventSlice';
 import calendarReducer from 'entities/calendar/calendarSlice';
 
-
 export const store = configureStore({
     reducer: {
         event: eventReducer,
