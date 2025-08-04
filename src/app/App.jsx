2b9cb6fa62cbf4f064f3500f8c0c2ad3a21da306
Pavlo Shulhin <pavelshulgin2009@gmail.com>
2025-08-04 19:23:58 -0400

    fixed 1.0

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import { Provider } from "react-redux";
 import DayGrid from "widgets/DayGrid/DayGrid";
 import DayView from "pages/DayView/DayView";
-import { store } from "Redux/store";
+import { store } from "../redux/store";
 function App() {
   return (
     <Provider store={store}>
